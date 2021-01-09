3cd6406248129cf4a25460d65af91a9344cd7150
Coolrelia <sammyrludwig@gmail.com>
2021-01-09 01:28:39 -0500

    Movement and Abilities

---
+++ Style Network/Assets/Player.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int topMost;
+    public int bottomMost;
+    public int leftMost;
+    public int rightMost;
+
+    public int health;
+    public char previousAttack;
+
+    public Animator anim;
+
+    private Vector3 pos;
+
+    StyleMeter sm;
+
+    private void Start()
+    {
+        sm = FindObjectOfType<StyleMeter>();
+        InvokeRepeating("ResetPreviousAttack", 0f, 5f);
+    }
+
+    private void Update()
+    {
+        //Abilities
+        BasicAttack();
+        Q();
+        W();
+        E();
+        R();
+
+
+        //Movement 
+        if(pos.y < topMost)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                pos.y += 1;
+                transform.position = pos;
+            }
+
+        }
+        if(pos.y > bottomMost)
+        {
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                pos.y -= 1;
+                transform.position = pos;
+            }
+        }
+        if(pos.x > leftMost)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                pos.x -= 1;
+                transform.position = pos;
+            }
+        }
+        if (pos.x < rightMost)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                pos.x += 1;
+                transform.position = pos;
+            }
+        }
+    }
+
+    void ResetPreviousAttack()
+    {
+        previousAttack = '0';
+    }
+
+    void BasicAttack()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("BasicAttack");
+            foreach(Enemy enemy in FindObjectsOfType<Enemy>())
+            {
+                if(transform.position.y == enemy.transform.position.y)
+                {
+                    if (previousAttack == 'B')
+                    {
+                        sm.rank -= 1;
+                    }
+                    else
+                    {
+                        sm.rank += 1;
+                    }
+                    previousAttack = 'B';
+                    enemy.health -= 10;
+                }
+            }
+        }
+    }
+
+    void Q()
+    {
+        if(sm.rank >= 1)
+        {
+            if(Input.GetKeyDown(KeyCode.Q))
+            {
+                anim.SetTrigger("Sword");
+                foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+                {
+                    if(enemy.transform.position.x == transform.position.x + 1 && enemy.transform.position.y == transform.position.y)
+                    {
+                        if (previousAttack == 'Q')
+                        {
+                            sm.rank -= 1;
+                        }
+                        else
+                        {
+                            sm.rank += 1;
+                        }
+                        previousAttack = 'Q';
+                        enemy.health -= 50;
+                    }
+                }
+            }
+        }
+    }
+
+    void W()
+    {
+        if(sm.rank >= 2)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                anim.SetTrigger("Sword");
+                foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+                {
+                    if(enemy.transform.position.y == transform.position.y && enemy.transform.position.x == transform.position.x + 1 ||
+                        enemy.transform.position.y == transform.position.y && enemy.transform.position.x == transform.position.x + 2)
+                    {
+                        if (previousAttack == 'W')
+                        {
+                            sm.rank -= 1;
+                        }
+                        else
+                        {
+                            sm.rank += 1;
+                        }
+                        previousAttack = 'W';
+                        enemy.health -= 80;
+                    }
+                }
+            }
+        }
+    }
+
+    void E()
+    {
+        if(sm.rank >= 4)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                anim.SetTrigger("Sword");
+                foreach (Enemy enemy in FindObjectsOfType<Enemy>())
+                {
+                    if(enemy.transform.position.y == transform.position.y && enemy.transform.position.x == transform.position.x + 1 ||
+                       enemy.transform.position.y == transform.position.y + 1 && enemy.transform.position.x == transform.position.x + 1 ||
+                       enemy.transform.position.y == transform.position.y - 1 && enemy.transform.position.x == transform.position.x + 1)
+                    {
+                        if (previousAttack == 'E')
+                        {
+                            sm.rank -= 1;
+                        }
+                        else
+                        {
+                            sm.rank += 1;
+                        }
+                        previousAttack = 'E';
+                        enemy.health -= 100;
+                    }
+                }
+            }
+        }
+    }
+
+    void R()
+    {
+        if (sm.rank >= 5)
+        {
+
+        }
+    }
+}
---
+++ Style Network/Assets/StyleMeter.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StyleMeter : MonoBehaviour
+{
+    public Sprite E;
+    public Sprite D;
+    public Sprite C;
+    public Sprite B;
+    public Sprite A;
+    public Sprite S;
+
+    public Sprite background;
+    public Sprite backgroundS;
+
+    public SpriteRenderer currentLetter;
+    public SpriteRenderer currentBackground;
+
+    public int rank;
+
+    private void Start()
+    {
+        InvokeRepeating("ResetStyleRank", 5f, 5f);
+    }
+
+    private void Update()
+    {
+        StyleMeterRank();
+    }
+
+    void ResetStyleRank()
+    {
+        rank--;
+    }
+
+    private void StyleMeterRank()
+    {
+        switch(rank)
+        {
+            case 0:
+                currentLetter.sprite = E;
+                currentBackground.sprite = background;
+                break;
+
+            case 1:
+                currentLetter.sprite = D;
+                currentBackground.sprite = background;
+                break;
+
+            case 2:
+                currentLetter.sprite = C;
+                currentBackground.sprite = background;
+                break;
+
+            case 3:
+                currentLetter.sprite = B;
+                currentBackground.sprite = background;
+                break;
+
+            case 4:
+                currentLetter.sprite = A;
+                currentBackground.sprite = background;
+                break;
+
+            case 5:
+                currentLetter.sprite = S;
+                currentBackground.sprite = backgroundS;
+                break;
+        }
+    }
+
+
+}
